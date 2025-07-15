b00892cf64619d3b6a07844fd32a8eb9f3c94f34
PeramatoG <luismanuelperamato@gmail.com>
2025-07-15 15:22:08 +0200

    The basic sections of the fixtures are displayed correctly.

--- gui/fixturetablepanel.cpp
+++ gui/fixturetablepanel.cpp
@@ -49,7 +49,7 @@ void FixtureTablePanel::ReloadData()
         wxString pos = wxString::Format("%.1f, %.1f, %.1f", posArr[0], posArr[1], posArr[2]);
 
         auto euler = MatrixUtils::MatrixToEuler(fixture.transform);
-        wxString rot = wxString::Format("%.1f°, %.1f°, %.1f°", euler[0], euler[1], euler[2]);
+        wxString rot = wxString::Format("%.1f\u00B0, %.1f\u00B0, %.1f\u00B0", euler[0], euler[1], euler[2]);
 
         row.push_back(name);
         row.push_back(fixtureID);
