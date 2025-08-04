6ff7aa351f0aa16bc16727161941a37103800bab
PeramatoG <luismanuelperamato@gmail.com>
2025-08-04 13:22:23 +0200

    Improve table printing style

--- gui/tableprinter.cpp
+++ gui/tableprinter.cpp
@@ -18,8 +18,13 @@ void Print(wxWindow* parent, wxDataViewListCtrl* table)
     std::vector<int> selCols = dlg.GetSelectedColumns();
     if (selCols.empty())
         return;
+
     wxHtmlEasyPrinting printer;
-    wxString html = "<html><body><table border=1 cellspacing=0 cellpadding=2><tr>";
+    printer.SetStandardFonts(8);
+    printer.SetMargins(10, 10, 10, 10, 2);
+    wxString html =
+        "<html><head><style>body{margin:5px;font-size:8pt;}table{border-collapse:collapse;}"
+        "tr:nth-child(even){background-color:#f2f2f2;}</style></head><body><table border=1 cellspacing=0 cellpadding=2><tr>";
     for (int c : selCols) {
         html += "<th>";
         html += table->GetColumn(c)->GetTitle();
