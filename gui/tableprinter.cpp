1e7ae62918b80b8b3376138721c2c78224497ceb
PeramatoG <luismanuelperamato@gmail.com>
2025-08-04 13:29:53 +0200

    Fix table printing margins and column widths

--- gui/tableprinter.cpp
+++ gui/tableprinter.cpp
@@ -2,6 +2,7 @@
 #include "columnselectiondialog.h"
 #include <wx/dataview.h>
 #include <wx/html/htmprint.h>
+#include <algorithm>
 
 namespace TablePrinter {
 
@@ -21,22 +22,42 @@ void Print(wxWindow* parent, wxDataViewListCtrl* table)
 
     wxHtmlEasyPrinting printer;
     printer.SetStandardFonts(8);
-    printer.SetMargins(10, 10, 10, 10, 2);
+
+    std::vector<size_t> maxWidths(selCols.size(), 0);
+    for (size_t i = 0; i < selCols.size(); ++i) {
+        int c = selCols[i];
+        maxWidths[i] = std::max(
+            maxWidths[i],
+            static_cast<size_t>(table->GetColumn(c)->GetTitle().Length()));
+    }
+    for (unsigned int r = 0; r < table->GetItemCount(); ++r) {
+        for (size_t i = 0; i < selCols.size(); ++i) {
+            int c = selCols[i];
+            wxVariant val;
+            table->GetValue(val, r, c);
+            maxWidths[i] = std::max(
+                maxWidths[i],
+                static_cast<size_t>(val.GetString().Length()));
+        }
+    }
+
     wxString html =
-        "<html><head><style>body{margin:5px;font-size:8pt;}table{border-collapse:collapse;}"
-        "tr:nth-child(even){background-color:#f2f2f2;}</style></head><body><table border=1 cellspacing=0 cellpadding=2><tr>";
-    for (int c : selCols) {
-        html += "<th>";
+        "<html><head><style>body{margin:10mm;font-size:8pt;}table{border-collapse:collapse;}"
+        "td,th{white-space:nowrap;}tr:nth-child(even){background-color:#f2f2f2;}</style></head><body><table border=1 cellspacing=0 cellpadding=2><tr>";
+    for (size_t i = 0; i < selCols.size(); ++i) {
+        int c = selCols[i];
+        html += wxString::Format("<th style=\"width:%zuch;\">", maxWidths[i]);
         html += table->GetColumn(c)->GetTitle();
         html += "</th>";
     }
     html += "</tr>";
     for (unsigned int r = 0; r < table->GetItemCount(); ++r) {
         html += "<tr>";
-        for (int c : selCols) {
+        for (size_t i = 0; i < selCols.size(); ++i) {
+            int c = selCols[i];
             wxVariant val;
             table->GetValue(val, r, c);
-            html += "<td>";
+            html += wxString::Format("<td style=\"width:%zuch;\">", maxWidths[i]);
             html += val.GetString();
             html += "</td>";
         }
