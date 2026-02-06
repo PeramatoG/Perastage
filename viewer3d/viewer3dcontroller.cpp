103e77ccadd93cebad77b37521b92816a067c808
Luisma Peramato <luismanuelperamato@gmail.com>
2026-02-06 18:59:22 +0100

    Fix 2D fixture fill colors when rendering mesh outlines

--- viewer3d/viewer3dcontroller.cpp
+++ viewer3d/viewer3dcontroller.cpp
@@ -2067,7 +2067,7 @@ void Viewer3DController::DrawMeshWithOutline(
           SetGLColor(0.0f, 1.0f, 0.0f);
         else if (selected)
           SetGLColor(0.0f, 1.0f, 1.0f);
-        DrawMeshWireframe(mesh, scale, captureTransform);
+        DrawMeshWireframe(mesh, scale, captureTransform, false);
       }
       glLineWidth(lineWidth);
       SetGLColor(0.0f, 0.0f, 0.0f);
@@ -2075,7 +2075,7 @@ void Viewer3DController::DrawMeshWithOutline(
     CanvasStroke stroke;
     stroke.color = {0.0f, 0.0f, 0.0f, 1.0f};
     stroke.width = lineWidth;
-    DrawMeshWireframe(mesh, scale, captureTransform);
+    DrawMeshWireframe(mesh, scale, captureTransform, true);
     if (m_captureCanvas && mode != Viewer2DRenderMode::Wireframe) {
       CanvasFill fill;
       fill.color = {r, g, b, 1.0f};
@@ -2159,7 +2159,8 @@ void Viewer3DController::DrawMeshWithOutline(
 void Viewer3DController::DrawMeshWireframe(
     const Mesh &mesh, float scale,
     const std::function<std::array<float, 3>(const std::array<float, 3> &)> &
-        captureTransform) {
+        captureTransform,
+    bool captureLines) {
   if (!m_captureOnly) {
     glBegin(GL_LINES);
     for (size_t i = 0; i + 2 < mesh.indices.size(); i += 3) {
@@ -2190,7 +2191,7 @@ void Viewer3DController::DrawMeshWireframe(
     }
     glEnd();
   }
-  if (m_captureCanvas) {
+  if (m_captureCanvas && captureLines) {
     CanvasStroke stroke;
     stroke.color = {0.0f, 0.0f, 0.0f, 1.0f};
     stroke.width = 1.0f;
