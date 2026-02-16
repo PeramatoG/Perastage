a016f9df5450a02e69ff9fa065428669eefd904a
Luisma Peramato <luismanuelperamato@gmail.com>
2026-02-16 19:30:43 +0100

    Fix truss model resolution after text rider import

--- core/riderimporter.cpp
+++ core/riderimporter.cpp
@@ -678,7 +678,6 @@ bool RiderImporter::ImportText(const std::string &text) {
 
     Truss &t = trussIt->second;
     if (IsRenderableTrussGeometry(t.symbolFile))
-    if (!t.symbolFile.empty())
       continue;
 
     Truss parsed;
