8119b2df61e116cba5fc2ea1ba18c56f32fd4130
Luisma Peramato <luismanuelperamato@gmail.com>
2026-02-18 09:48:12 +0100

    Fix fine-channel parsing in GDTF mode and DMX attribute detection

--- peraviz/native/src/dmx/gdtf_dimmer_resolver.cpp
+++ peraviz/native/src/dmx/gdtf_dimmer_resolver.cpp
@@ -137,31 +137,6 @@ struct ParsedAttribute {
     bool is_fine = false;
 };
 
-int parse_trailing_number(const std::string &text) {
-    if (text.empty()) {
-        return -1;
-    }
-
-    size_t end = text.size();
-    while (end > 0 && std::isspace(static_cast<unsigned char>(text[end - 1])) != 0) {
-        --end;
-    }
-    if (end == 0 || std::isdigit(static_cast<unsigned char>(text[end - 1])) == 0) {
-        return -1;
-    }
-
-    size_t begin = end;
-    while (begin > 0 && std::isdigit(static_cast<unsigned char>(text[begin - 1])) != 0) {
-        --begin;
-    }
-
-    const std::string digits = text.substr(begin, end - begin);
-    if (digits.empty()) {
-        return -1;
-    }
-    return std::atoi(digits.c_str());
-}
-
 ParsedAttribute parse_attribute_name(const std::string &raw_attribute) {
     ParsedAttribute parsed;
     const std::string lower = lower_ascii(trim_ascii(raw_attribute));
@@ -169,9 +144,8 @@ ParsedAttribute parse_attribute_name(const std::string &raw_attribute) {
         return parsed;
     }
 
-    const int trailing_number = parse_trailing_number(lower);
     if (lower.find("fine") != std::string::npos || lower.find("lsb") != std::string::npos ||
-        lower.find(" low") != std::string::npos || trailing_number >= 2) {
+        lower.find(" low") != std::string::npos) {
         parsed.is_fine = true;
     }
 
