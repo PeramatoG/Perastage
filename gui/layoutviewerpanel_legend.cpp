93f8fede327fbebff3205123667bfba47efd5308
Luisma Peramato <luismanuelperamato@gmail.com>
2026-02-23 16:00:03 +0100

    Reset legend symbol scale multipliers to 1.0

--- gui/layoutviewerpanel_legend.cpp
+++ gui/layoutviewerpanel_legend.cpp
@@ -46,8 +46,8 @@ namespace {
 constexpr double kLegendContentScale = 0.7;
 constexpr int kLegendSymbolSizePx =
     static_cast<int>(64 * kLegendContentScale);
-constexpr double kLegendFallbackSymbolScale = 1.5;
-constexpr double kLegendSvgSymbolScale = 0.5;
+constexpr double kLegendFallbackSymbolScale = 1.0;
+constexpr double kLegendSvgSymbolScale = 1.0;
 
 int SymbolViewRank(SymbolViewKind kind) {
   switch (kind) {
