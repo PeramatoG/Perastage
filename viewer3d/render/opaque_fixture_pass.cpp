92cf674d17f5aaef544f43fed0da2adc34828672
Luisma Peramato <luismanuelperamato@gmail.com>
2026-02-25 22:22:31 +0100

    Use fixture world matrix for mirrored winding detection

--- viewer3d/render/opaque_fixture_pass.cpp
+++ viewer3d/render/opaque_fixture_pass.cpp
@@ -243,6 +243,11 @@ void OpaqueFixturePass::Render(
           float m2[16];
           MatrixToArray(obj.transform, m2);
           controller.ApplyTransform(m2, false);
+
+          const Matrix worldTransform = MatrixUtils::Multiply(f.transform, obj.transform);
+          float worldMatrix[16];
+          MatrixToArray(worldTransform, worldMatrix);
+
           auto applyCapture =
               [fixtureTransform, objTransform = obj.transform](
                   const std::array<float, 3> &p) {
@@ -261,7 +266,7 @@ void OpaqueFixturePass::Render(
           controller.DrawMeshWithOutline(obj.mesh, partR, partG, partB,
                                          RENDER_SCALE, highlight, selected, cx,
                                          cy, cz, wireframe, mode, applyCapture,
-                                         drawUnlit);
+                                         drawUnlit, worldMatrix);
           glPopMatrix();
         }
       } else {
