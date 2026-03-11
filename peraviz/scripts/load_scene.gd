14affa078147955bbb6f6767e62ad32d73da2ba1
Luisma Peramato <luismanuelperamato@gmail.com>
2026-03-11 19:50:18 +0100

    Avoid beam rebuilds for rotation-only gobo updates

--- peraviz/scripts/load_scene.gd
+++ peraviz/scripts/load_scene.gd
@@ -1690,12 +1690,8 @@ func _apply_emitter_light_state(light: SpotLight3D, photometric: Dictionary, nor
 	if _fixture_gobo_projector != null:
 		var gobo_controls: Dictionary = BeamOpticsControllerScript.BuildGoboControls(controls, _visual_settings, beam_defaults)
 		gobo_projection_changed = _fixture_gobo_projector.apply_gobo_projection(light, gobo_controls)
-		var previous_applied_gobo_rotation_deg: float = float(light.get_meta("peraviz_beam_last_gobo_rotation_deg", beam_params.get("gobo_rotation_deg", 0.0)))
 		var applied_gobo_rotation_deg: float = float(light.get_meta("peraviz_gobo_applied_rotation_deg", beam_params.get("gobo_rotation_deg", 0.0)))
-		light.set_meta("peraviz_beam_last_gobo_rotation_deg", applied_gobo_rotation_deg)
 		beam_params["gobo_rotation_deg"] = applied_gobo_rotation_deg
-		if absf(applied_gobo_rotation_deg - previous_applied_gobo_rotation_deg) > 0.001:
-			gobo_projection_changed = true
 	if gobo_projection_changed:
 		_cleanup_light_beam_renderers(light)
 	light.light_volumetric_fog_energy = float(_visual_settings.get("light_volumetric_fog_energy", 12.0)) * float(_visual_settings.get("haze_density_multiplier", 0.22))
