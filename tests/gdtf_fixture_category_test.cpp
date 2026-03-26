7eb292e78f8d6f7d42814415232bda2e65d2725e
Luisma Peramato <luismanuelperamato@gmail.com>
2026-03-26 19:13:16 +0100

    Improve wash inference from GDTF DMX channel attributes

--- tests/gdtf_fixture_category_test.cpp
+++ tests/gdtf_fixture_category_test.cpp
@@ -14,7 +14,8 @@ namespace fs = std::filesystem;
 static std::string WrapDescription(const std::string &name,
                                    const std::string &geometries,
                                    const std::string &attributes,
-                                   const std::string &emitters = "") {
+                                   const std::string &emitters = "",
+                                   const std::string &extraFixtureContent = "") {
   return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
          "<GDTF DataVersion=\"1.2\">"
          "<FixtureType Name=\"" +
@@ -25,6 +26,7 @@ static std::string WrapDescription(const std::string &name,
          attributes +
          "</Attributes></AttributeDefinitions>"
          "<Geometries>" + geometries + "</Geometries>"
+         extraFixtureContent +
          "<PhysicalDescriptions><Emitters>" + emitters +
          "</Emitters></PhysicalDescriptions>"
          "</FixtureType></GDTF>";
@@ -99,6 +101,17 @@ int main() {
                        "<Geometry Name=\"Base\"/><Geometry Name=\"Yoke\"/><Geometry Name=\"Head\"/><Beam Name=\"Beam\" BeamType=\"Spot\" BeamAngle=\"12\"/>",
                        ""),
        "Unknown"},
+      {"moving_wash_from_dmx_channels.gdtf",
+       WrapDescription("Mover 440",
+                       "<Geometry Name=\"Root\"/><Beam Name=\"Beam\" BeamType=\"Wash\" BeamAngle=\"30\"/>",
+                       "",
+                       "",
+                       "<DMXModes><DMXMode Name=\"8ch\"><DMXChannels>"
+                       "<DMXChannel Offset=\"1,1\"><LogicalChannel Attribute=\"Pan\"/></DMXChannel>"
+                       "<DMXChannel Offset=\"2,1\"><LogicalChannel Attribute=\"Tilt\"/></DMXChannel>"
+                       "<DMXChannel Offset=\"3,1\"><LogicalChannel Attribute=\"Zoom\"/></DMXChannel>"
+                       "</DMXChannels></DMXMode></DMXModes>"),
+       "Wash"},
       {"static_conventional.gdtf",
        WrapDescription("Fresnel 2k",
                        "<Geometry Name=\"Root\"/><Beam Name=\"Beam\" BeamType=\"Fresnel\" BeamAngle=\"35\"/>",
