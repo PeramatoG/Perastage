bc4ac1f21587ee12b9fbf43e5d2c87719ee88d99
Luisma Peramato <luismanuelperamato@gmail.com>
2026-03-28 22:19:49 +0100

    Fix Windows OpenGL header include prerequisites

--- viewer2d/viewer2d_ruler_overlay.cpp
+++ viewer2d/viewer2d_ruler_overlay.cpp
@@ -1,5 +1,11 @@
 #include "viewer2d_ruler_overlay.h"
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#endif
+
 #ifdef __APPLE__
 #define GL_SILENCE_DEPRECATION
 #include <OpenGL/gl.h>
